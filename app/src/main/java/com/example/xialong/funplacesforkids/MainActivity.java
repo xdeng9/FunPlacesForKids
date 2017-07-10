119e5294302e8954e81bf5e33037c1e5aac7f13a
Xialong Deng <xdeng9@gmail.com>
2017-07-09 21:02:27 -0700

    Add location detection

--- app/src/main/java/com/example/xialong/funplacesforkids/MainActivity.java
+++ app/src/main/java/com/example/xialong/funplacesforkids/MainActivity.java
@@ -1,33 +1,59 @@
 package com.example.xialong.funplacesforkids;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.example.xialong.funplacesforkids.util.Util;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE=100;
+    private TextView currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        currentLocation = (TextView) findViewById(R.id.current_location);
         setSupportActionBar(toolbar);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        checkPermission();
+        Location location = Util.getLocation(MainActivity.this);
+        if(location!=null){
+            updateLocation(location.getLatitude(), location.getLongitude());
         }
 
         mGoogleApiClient = new GoogleApiClient
@@ -44,8 +70,78 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         toggle.syncState();
     }
 
+    private void checkPermission(){
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE);
+        }
+    }
+
+    private void updateLocation(double lat, double lon){
+        currentLocation.setText(Util.getCity(this, lat, lon));
+    }
+
+    @Override
+    protected void onStart(){
+        Log.d(TAG, "onStart called");
+        super.onStart();
+        if(mGoogleApiClient!=null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mGoogleApiClient!=null && mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+    }
+
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed.");
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected called");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setInterval(36000); // Update location every hour
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+                return;
+            }
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        updateLocation(location.getLatitude(), location.getLongitude());
     }
 }
---
+++ app/src/main/java/com/example/xialong/funplacesforkids/util/Util.java
@@ -0,0 +1,54 @@
+package com.example.xialong.funplacesforkids.util;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
+
+import com.example.xialong.funplacesforkids.MainActivity;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by administrator on 7/9/17.
+ */
+
+public class Util {
+
+    public static String getCity(Context context, double lat, double lon){
+        String city="";
+
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses;
+        try{
+            addresses = geocoder.getFromLocation(lat, lon, 1);
+            if(addresses.size()>0){
+                city = addresses.get(0).getLocality();
+            }else{
+                city = "Unknown";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return city;
+    }
+
+    public static Location getLocation(Context context) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        int permissionCheck = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        Location location = null;
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            location = locationManager.getLastKnownLocation(bestProvider);
+        }
+        return location;
+    }
+}
