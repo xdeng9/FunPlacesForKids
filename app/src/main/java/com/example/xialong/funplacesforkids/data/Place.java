ec9f18b98b97b3e9f6e5e4d3db79e98f999df6cf
Joseph Deng <xdeng9@gmail.com>
2017-08-04 22:48:46 -0700

    Update Place class

--- app/src/main/java/com/example/xialong/funplacesforkids/data/Place.java
+++ app/src/main/java/com/example/xialong/funplacesforkids/data/Place.java
@@ -1,4 +1,16 @@
 package com.example.xialong.funplacesforkids.data;
 
 public class Place {
+
+    private String placeName;
+    private String placeImageUrl;
+    private String placeRating;
+    private String placeAddress;
+
+    public Place(String placeName, String placeImageUrl, String placeRating, String placeAddress){
+        this.placeName = placeName;
+        this.placeImageUrl = placeImageUrl;
+        this.placeRating = placeRating;
+        this.placeAddress = placeAddress;
+    }
 }
