71b09eaed29e60cf580b0c7822fabe6e5340d2f1
Xialong Deng <xdeng9@gmail.com>
2017-09-11 23:04:43 -0700

    Update Provider

--- app/src/main/java/com/example/xialong/funplacesforkids/data/PlaceProvider.java
+++ app/src/main/java/com/example/xialong/funplacesforkids/data/PlaceProvider.java
@@ -2,12 +2,27 @@ package com.example.xialong.funplacesforkids.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class PlaceProvider extends ContentProvider{
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private static final int PLACE = 1;
+    private static final int PLACE_WITH_ID = 2;
+    private PlaceDbHelper mDbHelper;
+
+    private static UriMatcher buildUriMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        String authority = PlaceContract.CONTENT_AUTHORITY;
+        uriMatcher.addURI(authority, PlaceContract.PATH, PLACE);
+        uriMatcher.addURI(authority, PlaceContract.PATH + "/*", PLACE_WITH_ID);
+
+        return uriMatcher;
+    }
     @Override
     public boolean onCreate() {
         return false;
