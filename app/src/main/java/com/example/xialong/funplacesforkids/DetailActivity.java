a05e5f3cfe42282d6624b896b3d78c78f739f685
josephdeng <xdeng9@gmail.com>
2017-10-15 03:04:10 -0700

    Update DetailActivity

--- app/src/main/java/com/example/xialong/funplacesforkids/DetailActivity.java
+++ app/src/main/java/com/example/xialong/funplacesforkids/DetailActivity.java
@@ -1,13 +1,21 @@
 package com.example.xialong.funplacesforkids;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.xialong.funplacesforkids.data.Place;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private Place mPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        mPlace = intent.getExtras().getParcelable("key");
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(mPlace.getPlaceName());
+        loadBackdrop();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +39,11 @@ public class DetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load(mPlace.getPlaceImageUrl()).centerCrop().into(imageView);
     }
 
 }
