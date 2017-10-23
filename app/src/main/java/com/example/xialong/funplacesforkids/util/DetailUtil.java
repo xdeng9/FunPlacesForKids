e84ebb5819a812863947785187e90dbb110c0fec
Xialong Deng <xdeng9@gmail.com>
2017-10-22 23:46:21 -0700

    Update DetailUtil

--- app/src/main/java/com/example/xialong/funplacesforkids/util/DetailUtil.java
+++ app/src/main/java/com/example/xialong/funplacesforkids/util/DetailUtil.java
@@ -1,4 +1,24 @@
 package com.example.xialong.funplacesforkids.util;
 
-public class DetailUtil {
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class DetailUtil extends AsyncTask<String, Void, String> {
+
+    private static final String KEY = "&key=AIzaSyAj4OYy0O9hkAPpgc7jzpc5LpwgpGGJkb8";
+    private Context mContext;
+
+    private DetailUtil(Context context){
+        mContext = context;
+    }
+
+    @Override
+    protected String doInBackground(String... args) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
 }
